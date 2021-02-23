3f51a367b89dc6ada0a4eca2ab39fe8681827be3
ikeacat <mfryk268@gmail.com>
2021-02-23 09:52:51 -0600

    basically done

--- js/loadProfile.js
+++ js/loadProfile.js
@@ -1,5 +1,34 @@
+const nalaImages = 7;
+const majorImages = 5;
+const simbaImages = 4;
+
 function unknownDog() {
     document.getElementById('profstamp').innerHTML = '<h4 style="color: red;" roboto>No recognizable dog was specified in the url. Click the header to go back to the homepage.</h4>'
+}
+
+function doPhotos(dog) {
+    var imagePath;
+    var amntOfImages;
+    if(dog == 'nala') {
+        imagePath = 'img/nala/';
+        amntOfImages = nalaImages;
+    } else if(dog == 'simba') {
+        imagePath = 'img/simba/';
+        amntOfImages = simbaImages;
+    } else if(dog == 'major') {
+        imagePath = 'img/major/';
+        amntOfImages = majorImages;
+    }
+    var i;
+    for(i = 0; i <= amntOfImages - 1; i++) {
+        if(i == 0) {
+            document.getElementById('picshere').innerHTML += '<div class="dogpic" first><img src="' + imagePath + '1.jpg"></div>'
+        } else {
+            var imageNum = i + 1;
+            imageNum = imageNum.toString();
+            document.getElementById('picshere').innerHTML += '<div class="dogpic"><img src="' + imagePath + imageNum + '.jpg"></div>'
+        }
+    }
 }
 
 window.onload = function() {
@@ -9,16 +38,19 @@ window.onload = function() {
             // Input the profile pic.
             document.getElementById('dog-name-under').innerHTML = 'Nala';
             document.getElementsByClassName('placeholdimg')[0].src = 'img/profpics/nala.jpg';
+            doPhotos('nala')
         } else if(window.location.href.includes('?dog=major')) {
             console.log('majey');
             // Input the profile pic.
             document.getElementById('dog-name-under').innerHTML = 'Major';
             document.getElementsByClassName('placeholdimg')[0].src = 'img/profpics/major.jpg';
+            doPhotos('major')
         } else if(window.location.href.includes('?dog=simba')) {
             console.log('simbey')
             // Input the profile pic.
             document.getElementById('dog-name-under').innerHTML = 'Simba'
             document.getElementsByClassName('placeholdimg')[0].src = 'img/profpics/simba.jpg';
+            doPhotos('simba');
         } else {
             console.error('Unrecognized dog.')
             unknownDog();
